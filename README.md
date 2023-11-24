547fc27510dac08622f327e633584694870ad421
Joaquin Urbano Serra <kimbalear@gmail.com>
2023-11-24 13:03:31 +0000

    update

--- README.md
+++ README.md
@@ -1,9 +1,19 @@
 sass --watch scss/main.scss:css/main.css
 
+ALM-Terms | en es pt
 
+SRH Services = Tab1
+People Reached = Tab2
+Commodities = Tab3
+Comprehensive Sexuality Education = Tab4
+Other Information & Education Sessions = Tab5
 
-
-
+Services and service subcategories = Tab1grid1c1
+Static Clinics (CL) = Tab1grid1c2
+Associated Clinics (AS) = Tab1grid1c3
+Community-Based Distribution / Services (CB) = Tab1grid1c4
+Mobile Outreach Units (OU) = Tab1grid1c5
+Digital Platforms (VC) = Tab1grid1c6
 
 
 
--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,8 @@
 $(document).ready(function () {
+  $("body").on("focus", "input", function (event) {
+    $(this).attr("autocomplete", "off");
+  });
+
   var firstTab = $(".tablinks:first");
   firstTab.addClass("active");
   $("#" + firstTab.attr("data-target")).show();
@@ -16,25 +20,120 @@ $(document).ready(function () {
 
   var tooltips = {
     tooltip1: `Lorem ipsum 1`,
-    tooltip2: `Lorem ipsum 2`
-};
+    tooltip2: `Lorem ipsum 2`,
+  };
 
-$(".tooltip-target").each(function() {
+  $(".tooltip-target").each(function () {
     var tooltipKey = $(this).data("tooltip-key");
     $(this).attr("data-tooltip", tooltips[tooltipKey]);
-});
-
-$(".tooltip-target").mouseenter(function() {
-    var tooltipContent = $(this).attr("data-tooltip");
-    $('<div class="tooltip-content">' + tooltipContent + '</div>').appendTo(this);
-}).mouseleave(function() {
-    $(this).find(".tooltip-content").remove();
-});
-
-
-  $('#toggleBox').click(function() {
-    $('#content').toggle();
-    $('.arrow').toggleClass('up');
   });
 
+  $(".tooltip-target")
+    .mouseenter(function () {
+      var tooltipContent = $(this).attr("data-tooltip");
+      $('<div class="tooltip-content">' + tooltipContent + "</div>").appendTo(
+        this
+      );
+    })
+    .mouseleave(function () {
+      $(this).find(".tooltip-content").remove();
+    });
+
+  $("#toggleBox").click(function () {
+    $("#content").toggle();
+    $(".arrow").toggleClass("up");
+  });
+
+  $(document).off('dhis2.de.event.formLoaded').on('dhis2.de.event.formLoaded', function () {
+    if ($("#CustomForm").length > 0) {
+        $("#tabs").tabs();
+        $("#CustomForm_Content").hide();
+        new RenderRadioBtnTags();
+        $("#loaderDiv").show();
+
+        new dsetTranslation(function () {
+            $("#loaderDiv").hide();
+            $("#CustomForm_Content").show();
+        });
+    }
+
+    // ============================================================================================================
+    // GCACITranslation
+
+    function dsetTranslation(_exeFunc) {
+        var me = this;
+
+        me.exeFunc = _exeFunc;
+
+        me.optionSetTermId = "xdBjO8vRHss";
+
+        me.loadedDataSetElementList = false;
+        me.loadedOptionSetList = false;
+
+        me.OPTION_KEY = "opt_c";
+
+
+        // ----------------------------------------------------------------------------------------------
+        // URLs
+
+        me.QUERY_URL_TERMS = "../api/optionSets/" + me.optionSetTermId +
+            ".json?fields=options[code,displayName]&paging=false";
+
+
+        // ----------------------------------------------------------------------------------------------
+        // HTML Element
+
+
+        me.tableTag = $("body");
+
+        // ----------------------------------------------------------------------------------------------
+        // Init method
+
+        me.init = function () {
+            console.log("INIT translation ... ");
+            me.translateOptionSetList();
+        }
+
+        // ----------------------------------------------------------------------------------------------
+        // Supportive methods
+
+        me.translateOptionSetList = function () {
+            var url = encodeURI(me.QUERY_URL_TERMS);
+            me.loadMetadata(url, function (response) {
+
+                var options = response.options;
+                for (var j in options) {
+                    var option = options[j];
+                    var value = option.displayName;
+                    me.tableTag.find("[keyword='" + me.OPTION_KEY + ":" + option.code + "']")
+                        .html(value);
+                }
+
+                me.exeFunc();
+            });
+        }
+
+        me.loadMetadata = function (url, exeFunc) {
+            $.ajax({
+                type: "GET",
+                url: url,
+                contentType: "application/json;charset=utf-8",
+                beforeSend: function (xhr) {
+                    //me.hideReportTag();
+                },
+                success: function (response) {
+                    exeFunc(response);
+                }
+            });
+
+        }
+
+
+        // ----------------------------------------------------------------------------------------------
+        // init
+
+        me.init();
+
+    }
+});
 });
